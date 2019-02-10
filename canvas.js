c0b8e7a6ee5315df2ee019b30f8ffa8aca903733
Adil Saju <adilsaju@gmail.com>
2019-02-11 00:23:23 +0530

    homework

--- canvas.js
+++ canvas.js
@@ -5,24 +5,34 @@ canvas.height = 800;
 var c = canvas.getContext('2d');
 
 
-var cx = 100;
-var cx2 = 100;
-var cx3 = 100;
+var cx3 = window.innerWidth;
+var cx = 0;
+var cx2 = window.innerWidth;
 var gamePts=0;
 function playButtonFn() {
     var playbutton = document.querySelector('#b1');
+    var scoreBoard=document.querySelector('#score-board')
     playbutton.addEventListener('click', function () {
         // console.log("hello world");
         // console.log(cx);
         // console.log(cx2);
         // console.log(cx3);
         if(Math.abs(cx2-cx3)<15&&Math.abs(cx-cx2)<35 && Math.abs(cx-cx3)<35 )
-        {
+        {   
+            playbutton.style.background="green";
+            playbutton.innerHTML="Great job. Go on!";
             gamePts+=1;
+            if(gamePts==1)
+            scoreBoard.style.color="green";
+
             console.log("Congrats! You got 1 more point")
+        }else
+        {
+            playbutton.style.background="red";
+            playbutton.innerHTML="Please time correctly!";
         }
         console.log("Current Points: "+gamePts);
-
+        scoreBoard.innerHTML=`Current Score: ${gamePts}`;
     });
 
 }
@@ -32,31 +42,26 @@ function playButtonFn() {
 function animate() {
 
     c.clearRect(0, 0, window.innerWidth, window.innerHeight);
-    // ctx.clearRect(20, 20, 100, 50);
-    // c2.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
-    // c3.clearRect(0, 0, window.innerWidth, window.innerHeight);
-
+    
+    c.beginPath();
+    c.arc(cx3, 700, 50, 0, 2 * Math.PI);
+    cx3 -= 18;
+    if (cx3 < -window.innerWidth)
+        cx3 = -(cx3) ;
+    c.stroke();
 
     c.beginPath();
     c.arc(cx, 500, 50, 0, 2 * Math.PI);
-    cx += 8;
+    cx += 18;
     if (cx > window.innerWidth)
-        cx = -(cx) + 1900;
+        cx = -(cx); // 1900 is calibrated value ;)
     c.stroke();
 
     c.beginPath();
     c.arc(cx2, 300, 50, 0, 2 * Math.PI);
-    cx2 -= 8;
-    if (cx2 < -100)
-        cx2 = -(cx2) + 2000;
-    c.stroke();
-
-    c.beginPath();
-    c.arc(cx3, 700, 50, 0, 2 * Math.PI);
-    cx3 -= 16;
-    if (cx3 < -100)
-        cx3 = -(cx3) + 2000;
+    cx2 -= 18;
+    if (cx2 < -window.innerWidth)
+        cx2 = -(cx2) ;
     c.stroke();
 
     requestAnimationFrame(animate);
